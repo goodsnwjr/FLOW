55563754951390e59cb19d002dc2acda9fad60db
GomiLim <poil_chxn@naver.com>
2021-01-08 09:22:30 +0900

    update menu style

--- src/components/sideNave/index.tsx
+++ src/components/sideNave/index.tsx
@@ -1,8 +1,5 @@
 import React from 'react';
 
-//assets
-import styles from './main.module.css';
-
 //modules
 import {
   faBars,
@@ -21,76 +18,90 @@ import {
   faFolder,
 } from '@fortawesome/free-regular-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import styled from 'styled-components';
+import { Menu, Button } from 'antd';
 
+const SideStyle = styled.div`
+  width: 11rem;
+  display: inline-block;
+  border-right: 1px solid gray;
+  padding: 10px 20px;
+  box-sizing: border-box;
+  background: #fff;
+  ul {
+    margin-top: 1.5rem;
+    border: none;
+  }
+`;
 const SideNave = () => {
   return (
-    <div>
-      <div className={styles.side_newProject}>
-        <button> + 새 프로젝트</button>
+    <SideStyle>
+      <div style={{ textAlign: 'center', marginTop: '1rem' }}>
+        <Button> + 새 프로젝트</Button>
       </div>
-      <div className={styles.side_noraml}>
-        <button>
+      <Menu>
+        <Menu.Item>
           <FontAwesomeIcon icon={faBars} />
           전체
-        </button>
-        <button>
+        </Menu.Item>
+        <Menu.Item>
           <FontAwesomeIcon icon={faInbox} />
           미보관
-        </button>
-        <button>
+        </Menu.Item>
+        <Menu.Item>
           <FontAwesomeIcon icon={faEnvelope} />
           읽지않음
-        </button>
-        <button>
+        </Menu.Item>
+        <Menu.Item>
           <FontAwesomeIcon icon={faStar} />
           즐겨찾기
-        </button>
-      </div>
-      <div className={styles.side_viewCollected}>
+        </Menu.Item>
+      </Menu>
+      <Menu>
         <h3>모아보기</h3>
-        <button>
+        <Menu.Item>
           <FontAwesomeIcon icon={faCheck} />
           전체 업무
-        </button>
-        <button>
+        </Menu.Item>
+        <Menu.Item>
           <FontAwesomeIcon icon={faCalendar} />
           전체 일정
-        </button>
-        <button>
+        </Menu.Item>
+        <Menu.Item>
           <FontAwesomeIcon icon={faArchive} />
           전체 파일
-        </button>
-        <button>
+        </Menu.Item>
+        <Menu.Item>
           <FontAwesomeIcon icon={faBookmark} />
           담아둔 글
-        </button>
-        <button>
+        </Menu.Item>
+        <Menu.Item>
           <FontAwesomeIcon icon={faAt} />
           나를 지정
-        </button>
-        <button>
+        </Menu.Item>
+        <Menu.Item>
           <FontAwesomeIcon icon={faChalkboardTeacher} />내 게시물
-        </button>
-      </div>
-      <div className={styles.side_storageBox}>
+        </Menu.Item>
+      </Menu>
+      <Menu>
         <h3>보관함</h3>
-        <button>
+        <Menu.Item>
           <FontAwesomeIcon icon={faFolder} /> 마케팅
-        </button>
-        <button>
+        </Menu.Item>
+        <Menu.Item>
           <FontAwesomeIcon icon={faFolder} />
           디자인
-        </button>
-        <button>
+        </Menu.Item>
+        <Menu.Item>
           <FontAwesomeIcon icon={faFolder} />
           엔지니어링
-        </button>
-        <button>
+        </Menu.Item>
+        <Menu.Item>
           <FontAwesomeIcon icon={faEyeSlash} />
           숨김
-        </button>
-      </div>
-    </div>
+        </Menu.Item>
+      </Menu>
+    </SideStyle>
   );
 };
 
--- src/layouts/mainLayout/index.tsx
+++ src/layouts/mainLayout/index.tsx
@@ -12,29 +12,49 @@ import {
   faComment,
 } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import styled from 'styled-components';
+import { Layout, Menu } from 'antd';
+const { Header, Content } = Layout;
 
 interface Props {
   children: JSX.Element;
 }
 
+const HeaderStyle = styled(Header)`
+  div,
+  ul {
+    display: inline-block;
+  }
+  input {
+    width: 50%;
+    margin-left: 30px;
+    height: 60px;
+  }
+  ul {
+    display: flex;
+    color: #fff;
+    float: right;
+    list-style: none;
+  }
+
+  li {
+    margin: 0 10px;
+  }
+`;
+
 export const MainLayout = ({ children }: Props) => {
   return (
     <>
-      <header>
-        <div>
-          <h2>게시글 등록</h2>
-          {/* <a href="./main" target="_self">
-            <img src={logo} alt="유튜브 로고" className={styles.logo} />
-          </a> */}
-        </div>
-        <div>
-          <input
-            type="test"
-            name="search_video"
-            id="search_video"
-            placeholder="검색"
-          />
-        </div>
+      <HeaderStyle>
+        <a href="./main" target="_self">
+          FLOW
+        </a>
+        <input
+          type="test"
+          name="search_video"
+          id="search_video"
+          placeholder="검색"
+        />
         <ul>
           <li>
             <p>서비스 업그레이드</p>
@@ -49,11 +69,11 @@ export const MainLayout = ({ children }: Props) => {
             <FontAwesomeIcon icon={faUserCircle} />
           </li>
         </ul>
-      </header>
-      <section>
+      </HeaderStyle>
+      <Layout>
         <SideNav />
-        <div>{children}</div>
-      </section>
+        <Content>{children}</Content>
+      </Layout>
     </>
   );
 };
