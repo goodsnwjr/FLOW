199518f4f1bea8a28f5294c35a30c91e88986140
GomiLim <poil_chxn@naver.com>
2021-01-11 11:29:19 +0900

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,64 +1,21 @@
 import React from 'react';
 import 'antd/dist/antd.css';
 
-import mainPage from './pages/main';
-import contentPage from './pages/content';
+import { MainPage, ContentPage } from 'pages';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 import { routes } from 'routes';
 
 function App() {
-  // const projects = useSelector(state => state.products);
-  // const project = {
-  //   project: [
-  //     {
-  //       id: 1,
-  //       title: '[SI] 현대캐피탈-신차영업팀',
-  //       people: 21,
-  //       favorites: false,
-  //     },
-  //     {
-  //       id: 2,
-  //       title: '[아이엠폼] 공지',
-  //       people: 81,
-  //       favorites: false,
-  //     },
-  //     {
-  //       id: 3,
-  //       title: '[IMS] 통합 관리',
-  //       people: 25,
-  //       favorites: false,
-  //     },
-  //     {
-  //       id: 4,
-  //       title: '[SI] FLOW.TEAM',
-  //       people: 64,
-  //       favorites: false,
-  //     },
-  //     {
-  //       id: 5,
-  //       title: '1:1 문의하기',
-  //       people: 21,
-  //       favorites: false,
-  //     },
-  //     {
-  //       id: 6,
-  //       title: '[시스템1팀] 정리중',
-  //       people: 21,
-  //       favorites: false,
-  //     },
-  //   ],
-  // };
+  console.log(routes);
 
-  // localStorage.setItem('project', JSON.stringify(project.project));
   return (
     <BrowserRouter>
       <Switch>
-        <Route path={routes.main[0].path} component={mainPage} exact />;
-        <Route path={routes.content[0].path} component={contentPage} exact />;
+        <Route path={routes.main[0].path} component={MainPage} exact />;
+        <Route path={routes.content[0].path} component={ContentPage} exact />;
       </Switch>
     </BrowserRouter>
   );
 }
-
 export default App;
