2f7ef9726e9830a91cae47fe93d293654fe83e8c
이현성 <goodsnwjr2@naver.com>
2021-01-11 16:15:45 +0900

    lhs

---
+++ src/modules/project/favoriteProject.ts
@@ -0,0 +1,22 @@
+export const checkFavorit = (
+  e: any,
+  projectsList: any,
+  dispatch: any,
+  favorite: any
+) => {
+  const favoriteProjectList = [...projectsList].map((item) => {
+    console.log(e.target.parentNode.parentNode);
+    const itemId = Number(
+      e.target.parentNode.parentNode.getAttribute('data-id')
+    );
+    console.log(itemId);
+    if (item.id === itemId) {
+      return Object.assign({}, item, {
+        favorites: item.favorites === true ? false : true,
+      });
+    } else {
+      return item;
+    }
+  });
+  dispatch(favorite(favoriteProjectList));
+};
