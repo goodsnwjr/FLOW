758d092a38a93d4062a8383dfadb46051a1cd364
이현성 <goodsnwjr2@naver.com>
2021-01-12 11:16:01 +0900

    Revert "write"
    This reverts commit 390798020ddc8a00d65b4bf0718b0396fde4fa18.

--- src/components/MainBox/index.tsx
+++ src/components/MainBox/index.tsx
@@ -65,7 +65,7 @@ export const MainBox = ({ removeProject, checkFavorit }: removeProjectProps) => 
     'cornflowerblue',
   ];
 
-  const productList = useSelector(selectProjects);
+  const projectList = useSelector(selectProjects);
   const menu = useSelector(selectMenu);
 
   const showModal = (e: any) => {
@@ -87,8 +87,8 @@ export const MainBox = ({ removeProject, checkFavorit }: removeProjectProps) => 
     <ItemStyle>
       {menu === 'all' ? (
         <>
-          {productList &&
-            productList.map((items: any, index: any) => {
+          {projectList &&
+            projectList.map((items: any, index: any) => {
               console.log(items.favorites);
               return (
                 <ItemBoxStyle
@@ -123,7 +123,7 @@ export const MainBox = ({ removeProject, checkFavorit }: removeProjectProps) => 
             })}
         </>
       ) : (
-        productList
+        projectList
           .filter((item: any) => {
             return item.favorites;
           })
