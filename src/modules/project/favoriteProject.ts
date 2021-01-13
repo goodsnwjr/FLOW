530c9823f189c65a53fddee4c0f477157fa2ad09
이현성 <goodsnwjr2@naver.com>
2021-01-13 18:53:36 +0900

    feature/lhs-11

--- src/modules/project/favoriteProject.ts
+++ src/modules/project/favoriteProject.ts
@@ -1,17 +1,6 @@
-interface projectListProps {
-  favorites: boolean;
-  id: number;
-  mainColor: string;
-  participants: any;
-  title: string;
-}
-
 export const checkFavorit = (e: any, projectsList: any, dispatch: any, favorite: any) => {
-  console.log(favorite);
   const favoriteProjectList = [...projectsList].map((item) => {
-    console.log(e.target.parentNode.parentNode);
     const itemId = Number(e.target.parentNode.parentNode.getAttribute('data-id'));
-    console.log(itemId);
     if (item.id === itemId) {
       return Object.assign({}, item, {
         favorites: item.favorites === true ? false : true,
